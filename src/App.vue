2e99e77e0b272f9c013768ab59bf951b72ea068d
Vallentini <vallentinigabrielli.silva@gmail.com>
2024-03-08 16:39:43 -0300

    atividade finalizada

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,39 @@
 <script setup>
 //CONTADOR
-import{ref} from 'vue'
+import { ref, computed } from 'vue'
 let contador = ref(0)
 let contador2 = ref(0)
- function incrementar() {
-    contador.value++;
-  }
+function incrementar() {
+  contador.value++;
+}
 
-function decrementar(){
+function decrementar() {
   if (contador.value > 0) {
-      contador.value--;
-    }
+    contador.value--;
   }
+}
 function reiniciar(
 ) {
   contador.value = 0;
 }
 
 function incrementar2() {
-    contador2.value++;
-  }
+  contador2.value++;
+}
 
-function decrementar2(){
+function decrementar2() {
   if (contador2.value > 0) {
-      contador2.value--;
-    }
+    contador2.value--;
   }
+}
 function reiniciar2(
 ) {
   contador2.value = 0;
 }
+const verificaParImpar = computed(() => { return soma() % 2 == 0 });
 
 function soma() {
-  contador.value + contador2.value;
+  return contador.value + contador2.value;
 }
 //FORMS INFORMAÇÃO
 const nome = 'Vallentini'
@@ -45,24 +46,19 @@ function inverter(texto) {
 function saudacao() {
   return `Olá, ${nome}!`
 }
-const menor = '<div style="border-radius:20px; background-color: rgb(202, 59, 59); width: 500px; color:black; height: 30px; margin-top: 50px"><p style="margin-left:150px">maior que 10 é red</p></div>'
-const maior = '<div style="border-radius:20px; background-color: rgb(109, 231, 156); width: 500px; color:black; height: 30px; margin-top: 50px;"><p style="margin-left:150px">menor que 10 é verdin </p></div>'
+const verdadeiroOuFalso = ref(true)
+function mudaVerdadeiroOuFalso() {
+  verdadeiroOuFalso.value = !verdadeiroOuFalso.value
+}
+const menor = '<div style="border-radius:20px; background-color: rgb(202, 59, 59); width: 500px; color:black; height: 30px; margin-top: 50px"><p style="margin-left:150px">---------------------------</p></div>';
+const maior = '<div style="border-radius:20px; background-color: rgb(109, 231, 156); width: 500px; color:black; height: 30px; margin-top: 50px;"><p style="margin-left:150px">--------------------------</p></div>';
+const par = '<div style="border-radius:20px; background-color: rgb(109, 231, 156); width: 500px; color:black; height: 30px; margin-top: 50px;"><p style="text-align: center">par</p>';
+const impar = '<div style="border-radius:20px; background-color: rgb(202, 59, 59); width: 500px; color:black; height: 30px; margin-top: 50px;"><p style="text-align: center">impar</p>';
+const verdadeiro = '<div style="border-radius:20px; background-color: rgb(109, 231, 156); width: 500px; color:black; height: 30px; margin-top: 50px;"><p style="text-align: center">verdadeiro</p>';
+const falso = '<div style="border-radius:20px; background-color: rgb(202, 59, 59); width: 500px; color:black; height: 30px; margin-top: 50px;"><p style="text-align: center">falso</p>';
 </script>
 
 <template>
-
-  <div class="info">
-    <h1>Exemplo 1 - Mostrando informações na tela</h1>
-    <p class="text-forms">Nome: {{ nome }} </p>
-    <p class="text-forms">Idade: {{ idade }} anos</p>
-    <hr />
-    <p class="text-forms">Para completar 50 anos faltam: {{ 50 - idade }} anos</p>
-    <p class="text-forms">O nome tem {{ nome.length }} caracteres</p>
-    <p class="text-forms">O nome invertido é: {{ inverter(nome).toUpperCase() }} </p>
-    <hr />
-    <p class="text-forms"> Exemplo de saudação usando função</p>
-    <p class="text-forms"> {{ saudacao() }} </p>
-  </div>
 
   <div class="info">
     <p class="text">O valor do contador 1 é: {{ contador }}</p>
@@ -73,14 +69,20 @@ const maior = '<div style="border-radius:20px; background-color: rgb(109, 231, 1
     <button @click="incrementar2">Incrementar</button>
     <button @click="decrementar2">Decrementar</button>
     <button @click="reiniciar2">Reiniciar</button>
-    <div> <p>A soma é {{ soma }}</p></div>
-    <div v-if="contador > 10 " v-html="maior" ></div>
-  <div v-else v-html="menor"></div>
-</div>
+    <div>
+      <p>A soma é {{ soma() }}</p>
+    </div>
+    <span v-if="soma() > 10" v-html="maior"></span>
+    <span v-if="soma() < 10" v-html="menor"></span>
+    <span v-if="verificaParImpar" v-html="par"></span>
+    <span v-if="!verificaParImpar" v-html="impar"></span>
+    <span v-if="verdadeiroOuFalso == true" v-html="verdadeiro"></span>
+    <span v-if="verdadeiroOuFalso == false" v-html="falso"></span>
+    <button @click="mudaVerdadeiroOuFalso()">muda</button>
+  </div>
 </template>
 
 <style scoped>
-
 .info {
   background-color: rgb(30, 117, 98);
   padding: 20px 30px;
@@ -93,15 +95,16 @@ h1 {
   color: white;
   font-weight: bold;
   font-size: 1.5em;
-  
+
 }
 
 .text-forms {
   font-size: 20px;
-  font-family:'Times New Roman', Times, serif
+  font-family: 'Times New Roman', Times, serif
 }
+
 button:hover {
-    transform: scale(1.5);
+  transform: scale(1.5);
 
 }
 
@@ -124,7 +127,5 @@ button {
   font-size: 20px;
   font-style: normal;
   font-weight: bold
-
-
 }
 </style>
